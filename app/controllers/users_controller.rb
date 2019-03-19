0b6b78b09ec2e920c9a782f0ab28a4ae6490c4ec
Amelie Oller <amelieoller@gmail.com>
2019-03-19 16:18:50 -0700

    Create user singup logic and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	# rendering (erb :page) should happen from a get request, not from a post, patch or delete request (that's when we use redirect)
+
 	get '/login' do
 		erb :login
 	end
@@ -14,10 +16,19 @@ class UsersController < ApplicationController
 	end
 
 	get '/signup' do
-
+		erb :signup
 	end
 
-	get '/users/:id' do 
-		"user show route"
+	post '/users' do
+		@user = User.new(params)
+		if @user.save
+			redirect "/users/#{@user.id}"
+		end
+	end
+
+	get '/users/:id' do
+		@user = User.find_by(id: params[:id])
+		
+		erb :'/users/show'
 	end
 end
